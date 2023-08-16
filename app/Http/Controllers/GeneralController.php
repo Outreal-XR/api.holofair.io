0cd0690f2998b36417cb82b990cd09c392c337d9
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-08-16 11:01:50 +0300

    update the upload api to accept single request

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -22,9 +22,8 @@ class GeneralController extends Controller
     public function uploadPropImages(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            "images" => "array|required|min:1|max:100",
-            "images.*.key" => "required|string",
-            "images.*.file" => "required|string",
+            "key" => "required|string",
+            "image" => "required|string",
         ]);
 
         if ($validator->fails()) {
@@ -37,59 +36,55 @@ class GeneralController extends Controller
         $s3Destination = 'props/images/';
         $extension = '.png';
 
+        //give write and read permissions to the folder
+        chmod(public_path($localDecodedFilePath), 0777);
+
+
         if (!file_exists(public_path($localDecodedFilePath))) {
             mkdir(public_path($localDecodedFilePath), 0777, true);
         }
 
         //upload images to s3
-        $props = [];
-        foreach ($request->images as $image) {
 
-            $file = $image["file"];
-            $key = $image["key"];
+        $file = $request->image;
+        $key = $request->key;
 
-            $filename = $key . $extension;
-            $decodedContent = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $file));
+        $filename = $key . $extension;
+        $decodedContent = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $file));
 
-            $localFile = public_path($localDecodedFilePath . $filename);
+        $localFile = public_path($localDecodedFilePath . $filename);
 
-            //save the file locally
-            $save = file_put_contents($localFile, $decodedContent);
+        //save the file locally
+        $save = file_put_contents($localFile, $decodedContent);
 
-            if (!$save) {
-                return response()->json([
-                    'error' => 'failed to save the image locally'
-                ], 400);
-            }
-
-            try {
-                $path = Storage::disk('s3')->put($s3Destination . $filename, file_get_contents($localFile));
-
-                if ($path) {
-                    $url =  Storage::disk("s3")->url($s3Destination . $filename);
-                    $destination =
-                        str_replace("holofair-mena.s3.me-south-1.amazonaws.com", "cdn.holofair.net", $url);
-
-                    $props[] = [
-                        "file" => $filename,
-                        "url" => $destination
-                    ];
-                } else {
-                    return response()->json([
-                        'error' => 'failed to save the image in s3'
-                    ], 400);
-                }
-            } catch (S3Exception $e) {
-                return response()->json([
-                    'awsError' => $e->getMessage()
-                ], 400);
-            }
+        if (!$save) {
+            return response()->json([
+                'error' => 'failed to save the image locally'
+            ], 400);
         }
 
-        return response()->json([
-            "message" => "Images uploaded successfully",
-            "props" => $props
-        ], 200);
+        try {
+            $path = Storage::disk('s3')->put($s3Destination . $filename, file_get_contents($localFile));
+
+            if ($path) {
+                $url =  Storage::disk("s3")->url($s3Destination . $filename);
+                $destination =
+                    str_replace("holofair-mena.s3.me-south-1.amazonaws.com", "cdn.holofair.net", $url);
+
+                return response()->json([
+                    "filename" => $filename,
+                    'url' => $destination
+                ], 200);
+            } else {
+                return response()->json([
+                    'error' => 'failed to save the image in s3'
+                ], 400);
+            }
+        } catch (S3Exception $e) {
+            return response()->json([
+                'awsError' => $e->getMessage()
+            ], 400);
+        }
     }
 
     public function testBinary(Request $request)
