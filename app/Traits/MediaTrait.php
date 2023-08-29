7a5686a9bbfa7ceb7ec9140dd5526259f5b47af1
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-08-29 10:58:48 +0300

    fix file permissions

--- app/Traits/MediaTrait.php
+++ app/Traits/MediaTrait.php
@@ -7,6 +7,11 @@ trait MediaTrait
     public function uploadMedia($file, $path)
     {
         $filename = time() . "-" . $file->getClientOriginalName();
+
+        if (!file_exists(public_path($path))) {
+            mkdir(public_path($path), 0777, true);
+        }
+
         $file->move(public_path($path), $filename);
         return $path . '/' . $filename;
     }
